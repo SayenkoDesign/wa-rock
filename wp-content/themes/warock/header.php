a9cdcc4909b11edb8c84b0ab3a2d054d9899a64e
diaboloshogunate <rparker@kodama.design>
2017-04-02 13:57:46 -0700

    live updates

--- wp-content/themes/warock/header.php
+++ wp-content/themes/warock/header.php
@@ -14,6 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="format-detection" content="telephone=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
